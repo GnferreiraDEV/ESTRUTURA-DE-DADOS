c81da36e6534dfe9817539430bf248c0277a0cc1
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-18 10:16:31 -0300

    Update exemplo002.c

--- exemplo002.c
+++ exemplo002.c
@@ -6,14 +6,31 @@ typedef struct {
     int Denominador;
 } Fracao;
 
+//Protótipos das funções:
+
 Fracao criarFracao(int Num, int Dem);
 
 int mian() {
 
     system("cls");
 
-
+    Fracao F1 = criarFracao( 3, 7);
+    Fracao F2 = criarFracao( 16, 20);
+    Fracao F3 = criarFracao( -20, 10);
 
 
     return 0;
+}
+
+//Implementação das funções:
+
+Fracao criarFracao(int Num, int Den){
+
+    Fracao F;
+
+    F.Numerador = Num;
+    F.Denominador = Den;
+
+    return F;
+
 }
