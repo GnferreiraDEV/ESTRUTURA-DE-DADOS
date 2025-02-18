4991fd35dde0aa78312472c69e14a805a3c0bad5
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-18 10:39:53 -0300

    Update exemplo002.c

--- exemplo002.c
+++ exemplo002.c
@@ -8,7 +8,8 @@ typedef struct {
 
 //Protótipos das funções:
 
-Fracao criarFracao(int Num, int Dem);
+Fracao criarFracao( int Num, int Dem );
+Fracao simplificarFracao( Fracao F );
 
 int mian() {
 
@@ -16,7 +17,9 @@ int mian() {
 
     Fracao F1 = criarFracao( 3, 7);
     Fracao F2 = criarFracao( 16, 20);
-    Fracao F3 = criarFracao( -20, 10);
+    Fracao F3 = criarFracao( -20, 30);
+
+    printf(%d/%d\n)
 
 
     return 0;
@@ -33,4 +36,17 @@ Fracao criarFracao(int Num, int Den){
 
     return F;
 
+}
+
+Fracao simplificarFracao( Fracao F ) {
+    int Menor = ( F.Numerador < F.Denominador ) ? F.Numerador : F.Denominador;
+    int MDC = 1;
+
+    for(int i = 1; i <= abs(Menor); i++) 
+        if(F.Numerador %  i == 0 && F.Denominador % i == 0)
+        MDC = i;
+
+    F.Numerador = F.Numerador /MDC;
+    F.Denominador = F.Denominador /MDC;
+    return F;
 }
