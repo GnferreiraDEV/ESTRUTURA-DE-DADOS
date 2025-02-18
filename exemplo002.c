ff5a33b46a30d3cbf9f18000dfc9dcae5581d627
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-18 10:06:35 -0300

    Create exemplo002.c

---
+++ exemplo002.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int Numerador;
+    int Denominador;
+} Fracao;
+
+Fracao criarFracao(int Num, int Dem);
+
+int mian() {
+
+    system("cls");
+
+
+
+
+    return 0;
+}
