25e1bc0dd0ae7b7c16be5ad5082b92fec96bc663
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-18 09:47:01 -0300

    Exemplo001
    Exemplos de exercícios e problemas feito em laboratório durante a ministração de aula.

---
+++ exemplo001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+    int a, b;
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &a);
+   
+    b = a - 1;
+
+    printf("O antecessor de do n�mero: %d", b);
+
+    return 0;
+}
