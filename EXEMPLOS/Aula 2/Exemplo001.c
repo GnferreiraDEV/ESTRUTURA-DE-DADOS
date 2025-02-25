f26a65f9e699e9e82912f1ef5b128000041f4744
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-02-25 09:30:48 -0300

    UPDATE

---
+++ EXEMPLOS/Aula 2/Exemplo001.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+
+FILE * fp;
+
+fp = fopen("arquivo.txt", "w");
+
+if (fp == NULL) {
+    printf("ERRO: o arquivo não pode ser aberto!\n");
+
+    exit(1);
+}
+
+fclose(fp);
+
+return 0;
+
+}
