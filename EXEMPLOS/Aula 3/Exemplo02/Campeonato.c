6de06943a215b13c49848b7d4c4d8f57c0b7cf45
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-03-11 10:09:26 -0300

    UPDATE

--- EXEMPLOS/Aula 3/Exemplo02/Campeonato.c
+++ EXEMPLOS/Aula 3/Exemplo02/Campeonato.c
@@ -17,3 +17,28 @@ typedef struct {
     float Aproveitamento; // % de pontos conquistados em relação ao total de pontos disputados
 
 } Equipe;
+
+
+Equipe* LerDados(const char *Tabela2024) {
+
+    FILE *fp = fopen(Tabela2024, "r");
+    Equipe *X = malloc(20 * sizeof(Equipe));
+
+    int N = 0;
+    char Cabecalho[64];
+
+    fscanf(fp, "%s", Cabecalho);
+
+    while (fscanf(fp,"%d;%[^;];%[^;];%d;%d;%d;%d;%d;%d;%d;%d;", &X[N].Posicao, X[N].Estado,
+    X[N].Time, &X[N].Pontos,&X[N].J,&X[N].V,&X[N].E,&X[N].D,&X[N].GP,&X[N].GC,&X[N].SD ) ==
+    11) {
+
+    X[N].Aproveitamento = 100 * (float) X[N].Pontos / (float) (3 * X[N].J);
+    N++;
+    }
+
+    fclose(fp);
+
+return X;
+
+}
