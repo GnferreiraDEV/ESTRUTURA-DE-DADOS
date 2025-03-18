98ff7bd857a0e08834e076413c612b18bdfea7e0
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-03-18 09:46:43 -0300

    Update Exemplo002.c

--- EXEMPLOS/Aula 4/Exemplo002.c
+++ EXEMPLOS/Aula 4/Exemplo002.c
@@ -2,16 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
-int mian(){
+int main(){
 
+    int N = 100000;
+    int i;
+    int *V = (int *)malloc(N * sizeof(int));
     clock_t t0, tf;
     double TempoCPU;
 
+    srand(time(NULL));
+
     t0 = clock();
+
+    for(i = 0; i < N; i++) V[i] = rand();
+    for(i = 0; i < N; i++) printf("%d\t", V[i]);
 
     tf = clock();
 
+    
+
     TempoCPU = ((double)(tf - t0)) /  CLOCKS_PER_SEC;
+
+    printf("Tempo de CPU usado: %3.lf segundos\n", TempoCPU);
 
     return 0;
 
