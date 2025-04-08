e07c8f622a6a145a43aa086b28b2b21c1d3100a6
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-04-08 09:06:05 -0300

    UPDATE

--- EXEMPLOS/Aula 6/exemplo001.c
+++ EXEMPLOS/Aula 6/exemplo001.c
@@ -15,13 +15,44 @@ typedef struct{
 
 Lista * criarLista(int N);
 
-int main();
+int main(){
+    Lista *X = criarLista(15);
+
+    return 0;
+}
 
     Lista * criarLista(int N){
 
         Lista * L = (Lista *)malloc( sizeof (Lista));
+        
+        if (L == NULL){
+
+            printf("Erro ao alocar memoria na lista!\n");
+            return NULL;
+            
+        }
+
+        L->Array = (Item *)malloc( N * sizeof(Item));
+
+        if (L->Array == NULL){
+
+            printf("Erro ao alocar memoria no vetor!\n");
+            free(L);
+            return NULL;
+
+        }
+
+        L->tamanho = 0;
+        L->capacidade = N;
+
+        return L;
+        
+          
+        
+
 
     }
+
 
 
 
