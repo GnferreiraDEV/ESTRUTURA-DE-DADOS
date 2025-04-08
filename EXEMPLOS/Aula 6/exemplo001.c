64ae12e41c19dad3012435ffacbb8497e84bab01
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-04-08 10:00:41 -0300

    UPDATE

--- EXEMPLOS/Aula 6/exemplo001.c
+++ EXEMPLOS/Aula 6/exemplo001.c
@@ -16,6 +16,7 @@ typedef struct{
 Lista * criarLista(int N);
 void destruirLista(Lista * L);
 void inserirElemento(Lista *L, Item I, int Posicao);
+void imprimirLista(Lista *L);
 
 int main(){
     Lista *X = criarLista(15);
@@ -25,6 +26,23 @@ int main(){
         printf("A lista foi criada!\n");
         printf("%x\n", X);
     }
+
+    Item E1 = {10};
+    inserirElemento(X, E1, 0);
+    Item E2 = {15};
+    inserirElemento(X, E2, 1);
+    Item E3 = {20};
+    inserirElemento(X, E3, 2);
+    Item E4 = {25};
+    inserirElemento(X, E4, 3);
+    Item E5 = {30};
+    inserirElemento(X, E5, 4);
+    Item E6 = {35};
+    inserirElemento(X, E6, 5);
+    Item E7 = {40};
+    inserirElemento(X, E7, 6);
+
+    imprimirLista(X);
 
     destruirLista(X);
 
@@ -74,17 +92,31 @@ int main(){
             return;
         }
 
-        if(Posicao < 0 || Posicao >= L->tamanho){
+        if(Posicao == 0)L->Array[Posicao] = I;
+
+     else if(Posicao >= 0 && Posicao <= L->tamanho){
+
+    int i;
+    for(i = L->tamanho - 1; i >=Posicao; i--) L->Array[i + 1] = L->Array[i];
+    L->Array[Posicao] = I;
+
+        }else{
             printf("Erro a posicao eh invalida!\n");
             return;
         }
 
-        int i;
-        for(i = L->tamanho - 1; i >=Posicao; i--) L->Array[i + 1] = L->Array[i];
-        L->Array[Posicao] = I;
-
         L->tamanho++;
+
     }
+
+    void imprimirLista(Lista *L){
+        int i;
+        for (i = 0; i < L->tamanho; i++) printf("%d\n", L->Array[i].Chave);
+        printf("\n");
+    }
+
+
+    
 
 
 
